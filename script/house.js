e29ac0fdca781a964902910c469fb10e182f38f7
Jaumemasmorla <masjaume0@gmail.com>
2024-01-10 17:53:28 +0100

    house congressmen data display

--- script/house.js
+++ script/house.js
@@ -6,8 +6,49 @@ fetch(url, {
 })
 .then(response => response.json()) 
 .then(json => {
-    const  members = json.results[0].members;
-    console.log(members)
+    const  houseMembersArray = json.results[0].members;
+    buildTable(houseMembersArray)
 }
     
     ); 
+
+  document.getElementById('thead').innerHTML="";
+
+  let headers = ['Name', 'Affiliation', 'State', 'Years', 'Votes to Party'];
+
+  for (let i = 0; i < headers.length; i++){
+    let headersRow = document.createElement('th');
+    headersRow.textContent = headers[i];
+
+    document.getElementById('thead').append(headersRow);
+  }
+
+  function buildTable(houseMembersArray){
+
+    document.getElementById('tbody').innerHTML="";
+
+    for(let i = 0; i < houseMembersArray.length; i++){
+
+      let row = document.createElement('tr');
+
+      let link = document.createElement('a');
+
+      link.textContent = houseMembersArray[i].first_name + " " + (houseMembersArray[i].middle_name || "") + houseMembersArray[i].last_name;
+
+      link.setAttribute('href',houseMembersArray[i].url);
+
+      row.insertCell().append(link);
+
+      row.insertCell().innerHTML = houseMembersArray[i].party;
+
+      row.insertCell().innerHTML = houseMembersArray[i].state;
+
+      row.insertCell().innerHTML = houseMembersArray[i].seniority;
+
+      row.insertCell().innerHTML = houseMembersArray[i].votes_with_party_pct;
+
+      document.getElementById('tbody').append(row);
+
+    }
+
+  }
