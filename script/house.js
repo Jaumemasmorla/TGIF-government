e4b5731c20b3c8382190ec701daf75c200fde9f1
Jaumemasmorla <masjaume0@gmail.com>
2024-01-15 15:05:21 +0100

    house checkbox

--- script/house.js
+++ script/house.js
@@ -1,10 +1,6 @@
 /* hacer el checkbox */
 
-const parties = {
-  D: 'Democrat',
-  R: 'Republican',
-  ID: 'Independent'
-}
+let houseMembersArray;
 
 /* para dropdown import {string} from './holamundo.mjs'
 console.log(string) y  (en el fichero donde esta la informacion) export const  string ="hola mundo"*/
@@ -18,11 +14,12 @@ fetch(url, {
 })
 .then(response => response.json()) 
 .then(json => {
-    const  houseMembersArray = json.results[0].members;
-    buildTable(houseMembersArray)
+    houseMembersArray = json.results[0].members;
+    buildTable(houseMembersArray);
+    filteredTable();
+
 }
-    
-    ); 
+     ); 
 
   document.getElementById('thead').innerHTML="";
 
@@ -63,4 +60,41 @@ fetch(url, {
 
     }
 
+  };
+
+  const parties = {
+    D: 'Democrat',
+    R: 'Republican',
+    ID: 'Independent'
+  };
+
+  let entries = Object.entries(parties);
+
+  
+
+  let checkboxEntries = entries.map(([key, value]) => {
+    let input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('value', key);
+    input.addEventListener("change", filteredTable);
+
+    let label = document.createElement('label');
+    label.innerHTML = value;
+    document.getElementById('checkbox-container').appendChild(input);
+    document.getElementById('checkbox-container').appendChild(label);
+  } );
+function filteredTable(){
+
+  let checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+
+  if(checkboxes.length === 0){
+     buildTable(houseMembersArray)
+  }else{
+    let selectedParties = Array.from(checkboxes).map((checkbox) => checkbox.value );
+    let filteredMembers = houseMembersArray.filter((member) => selectedParties.includes(member.party));
+    buildTable(filteredMembers);
   }
+
+};
+
+/*Hacer el dropdown*/
